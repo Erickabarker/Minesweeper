131dbbaed7b3619cc63fafa9c68e8354306962a7
Erickabarker <110427318+Erickabarker@users.noreply.github.com>
2022-11-26 20:43:04 +0000

    set function loops for creating grid sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,66 @@
-const gameArea = document.getElementById("game-area");
+class Minesweeper{
+    constructor(opts ={}){
+        let loadedData ={};
 
-function gameType(rows, cols) {
-  gameSize(rows, cols);
-  applyStyle(rows, cols);
+        if (hasLocalStorage && localStorage["minesweeper.data"]){
+            loadedData = JSON.parse(localStorage["minesweeper.data"]);
+            this.loadGame = true;
+        }
+    }}
+
+        Object.assign(
+            this,
+            {
+                grid: [],
+                minesFound: 0,
+                falseMines: 0,
+                game_status: "Playing...",
+                playing: true,
+                movesMade: 0,
+                options:{
+                    rows:10,
+                    cols:10,
+                    mines:8
+                }
+            },
+            {options: opts},
+            loadedData
+        );
+
+        let rows = this.options["rows"];
+            if (isNaN(rows)) {
+                this.options["rows"] = 10;
+            } else if (rows < 3){
+                this.options["rows"] = 3;
+            } else if (rows > 20){
+                this.options["rows"] = 20;
+            }
+        
+            let cols = this.options["cols"];
+                if (isNaN(cols)) {
+                    this.options["cols"] = 10;
+                } else if (cols < 3){
+                    this.options["cols"] = 3;
+                } else if (cols > 20){
+                    this.options["cols"] = 20;
+                }  
+        
+            if (isNaN(this.options["mines"])) {
+                this.options["mines"] = 8;
+            } if (this.options["mines"] < 0) {
+                this.options["mines"] = 1;
+            } else if (this.options["mines"] >
+                this.options["rows"] * this.options["cols"]
+
+            ){
+                this.options["mines"] = this.options["rows"] * this.options["cols"];
+
 }
+if(this.loadGame){
+    this.load();
 
-function applyStyle(rows, cols) {
-  gameArea.classList.remove("beginner", "intermediate", "expert");
-  rows == 9 && cols == 9
-    ? gameArea.classList.add("beginner")
-    : rows == 16 && cols == 16
-    ? gameArea.classList.add("intermediate")
-    : rows == 30 && cols == 16
-    ? gameArea.classList.add("expert")
-    : "Test";
-}
+} else { 
+    this,
 
-function gameSize(rows, cols) {
-  clearGame();
-  for (square = 0; square < rows * cols; square++) {
-    let squareInner = document.createElement("div");
-    squareInner.innerText = square + 1;
-    gameArea.appendChild(squareInner).className = "ms-cell";
-  }
-}
-
-function clearGame() {
-  gameArea.innerHTML = "";
+this.save();
 }
